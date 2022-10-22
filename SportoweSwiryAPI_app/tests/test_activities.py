65e8139bfa10b4d6a51090811200a45539b332b3
LukBartsch <lukasz.bartsch@gmail.com>
2022-10-22 22:42:02 +0200

    Test functions - update activity

--- SportoweSwiryAPI_app/tests/test_activities.py
+++ SportoweSwiryAPI_app/tests/test_activities.py
@@ -284,3 +284,153 @@ def test_add_activity_invalid_distance(client, token, sample_activity):
     assert response.headers['Content-Type'] == 'application/json'
     assert response_data['success'] is False
     assert response_data['message']['distance'] == ['Not a valid number.']
+
+
+def test_update_activity(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            json={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2022',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 201
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is True
+    assert 'Bieg' in response_data['data'].values()
+    assert 3 in response_data['data'].values()
+    assert '22-10-2022' in response_data['data'].values()
+    assert '5.0' in response_data['data'].values()
+    assert '0:30:00' in response_data['data'].values()
+
+
+def test_update_activity_not_found(client, token, sample_activity):
+    response = client.put('/api/v1/activities/99',
+                            json={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2022',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 404
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'data' not in response_data
+    assert response_data['message'] == 'Activity with id 99 not found'
+
+
+def test_update_activity_missing_token(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            json={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2022',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 401
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert response_data['message'] == 'Missing token. Please login to get new token'
+
+
+def test_update_activity_invalid_conent_type(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            data={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2022',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 415
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert response_data['message'] == 'Content type must be application/json'
+
+
+@pytest.mark.parametrize(
+    'data,missing_field',
+    [
+        ({'activity_name': 'Bieg', 'date': '22-10-2022'}, 'distance'),
+        ({'activity_name': 'Bieg', 'distance': '5'}, 'date'),
+        ({'distance': '5', 'date': '22-10-2022'}, 'activity_name')
+    ]
+)
+def test_update_activity_missing_data(client, token, data, missing_field):
+    response = client.put('/api/v1/activities/1',
+                            json=data,
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert missing_field in response_data['message']
+    assert 'Missing data for required field.' in response_data['message'][missing_field]
+
+
+def test_update_activity_invalid_sport_name(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            json={
+                                'activity_name': 'Wrong_name',
+                                'date': '22-10-2022',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert response_data['message']['activity_name'] == ['This type of activity (Wrong_name) is not available in the application.']
+
+
+def test_update_activity_invalid_date(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            json={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2122',
+                                'distance': '5',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert response_data['message']['date'] == [f'Birth date must be lower than {datetime.now().date()}']
+
+
+def test_update_activity_invalid_distance(client, token, sample_activity):
+    response = client.put('/api/v1/activities/1',
+                            json={
+                                'activity_name': 'Bieg',
+                                'date': '22-10-2022',
+                                'distance': 'five',
+                                'time': '0:30:00'
+                            },
+                            headers={
+                                'Authorization': f'Bearer {token}'
+                            })
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert response_data['message']['distance'] == ['Not a valid number.']
